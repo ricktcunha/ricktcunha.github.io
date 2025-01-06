d3727d273eb8f39e6bfeff78dc07d69aa9a14c2c
RickTCunha <ricktcunha@gmail.com>
2025-01-05 23:23:21 -0300

    att hedaer funcionalidades

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,12 @@
 // Seleciona o ícone do hambúrguer e o menu
-const hamburger = document.getElementById("hamburger");
-const menu = document.querySelector(".header-menu");
+const hamburger = document.getElementById('hamburger');
+const menu = document.querySelector('.header-menu');
 
-hamburger.addEventListener("click", () => {
-  menu.classList.toggle("active"); // Abre/fecha o menu
-  hamburger.classList.toggle("active"); // Altera o estado do ícone
+hamburger.addEventListener('click', () => {
+  menu.classList.toggle('active'); // Abre/fecha o menu
+  hamburger.classList.toggle('active'); // Altera o estado do ícone
 });
+
 
 // Adiciona a classe 'fade-in' ao carregar a página
 document.addEventListener("DOMContentLoaded", () => {
